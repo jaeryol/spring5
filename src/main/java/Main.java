403c9f6f0663ea1d49997a854b8de2d04c4f4ea2
Jaeryong Im <hackerrs.im@gmail.com>
2019-04-17 15:12:16 +0900

    Chapter03 스프링 DI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,134 @@
+import assembler.Assembler;
+import config.AppConf1;
+import config.AppConf2;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static ApplicationContext ctx = null;
 
-        AnnotationConfigApplicationContext ctx =
-                new AnnotationConfigApplicationContext(AppContext.class);
+    public static void main(String[] args) throws IOException {
+        ctx = new AnnotationConfigApplicationContext(AppConf1.class, AppConf2.class);
 
-        Greeter g1 = ctx.getBean("greeter", Greeter.class);
-        Greeter g2 = ctx.getBean("greeter", Greeter.class);
+        BufferedReader reader =
+            new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("(g1 == g2) = " + (g1 == g2));
+        while (true) {
+            System.out.println("명령어를 입력하세요: ");
+            String command = reader.readLine();
 
-        ctx.close();
+            if (command.equalsIgnoreCase("exit")) {
+                System.out.println("종료합니다.");
+                break;
+            }
+
+            if (command.startsWith("new ")) {
+                processNewCommand(command.split(" "));
+                continue;
+            } else if (command.startsWith("change ")) {
+                processChangeCommand(command.split(" "));
+                continue;
+            } else if (command.equals("list")) {
+                processListCommand();
+                continue;
+            } else if (command.startsWith("info ")) {
+                processInfoCommand(command.split(" "));
+                continue;
+            } else if (command.equals("version")) {
+                processVersionCommand();
+                continue;
+            }
+
+            printHelp();
+        }
+    }
+
+    private static void processVersionCommand() {
+        VersionPrinter versionPrinter =
+            ctx.getBean("versionPrinter", VersionPrinter.class);
+        versionPrinter.print();
+    }
+
+    private static void processInfoCommand(String[] args) {
+        if (args.length != 2) {
+            printHelp();
+            return;
+        }
+        MemberInfoPrinter infoPrinter =
+            ctx.getBean("infoPrinter", MemberInfoPrinter.class);
+        infoPrinter.printMemberInfo(args[1]);
+    }
+
+    private static Assembler assembler = new Assembler();
+
+    private static void processNewCommand(String[] args) {
+        System.out.println((args.length));
+        if (args.length != 5) {
+            printHelp();
+            return;
+        }
+
+        MemberRegisterService registerService =
+            ctx.getBean("memberRegisterService", MemberRegisterService.class);
+
+        RegisterRequest req = new RegisterRequest();
+        req.setEmail(args[1]);
+        req.setName(args[2]);
+        req.setPassword(args[3]);
+        req.setConfirmPassword(args[4]);
+
+        if (!req.isPasswordEqualToConfirmPassword()) {
+            System.out.println("암호와 확인이 일치하지 않습니다.");
+            return;
+        }
+
+        try {
+            registerService.regist(req);
+            System.out.println("등록했습니다.\n");
+        } catch (DuplicateMemberException e) {
+            System.out.println("이미 존재하는 이메일입니다. \n");
+        }
+
+    }
+
+    private static void processChangeCommand(String[] args) {
+        System.out.println((args.length));
+        if (args.length != 4) {
+            printHelp();
+            return;
+        }
+
+        ChangePasswordService changePasswordService =
+            ctx.getBean("changePasswordService", ChangePasswordService.class);
+
+        try {
+            changePasswordService.changePassword(args[1], args[2], args[3]);
+            System.out.println("암호를 변경했습니다. \n");
+        } catch (MemberNotFoundException e) {
+            System.out.println("존재하지 않는 이메일입니다. \n");
+        } catch (WrongIdPasswordException e) {
+            System.out.println("이메일과 암호가 일치하지 않습니다. \n");
+        }
+    }
+
+    private static void processListCommand() {
+        MemberListPrinter printer =
+            ctx.getBean("listPrinter", MemberListPrinter.class);
+        printer.printAll();
+    }
+
+    private static void printHelp() {
+        System.out.println();
+        System.out.println("잘못된 명령입니다.아래 명령어 사용법을 확인하세요.");
+        System.out.println("명령어 사용법");
+        System.out.println("new 이메일 이름 암호 암호확인");
+        System.out.println("change 이메일 현재비번 변경비번");
+        System.out.println();
     }
 }
---
+++ src/main/java/config/AppConf2.java
@@ -0,0 +1,54 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.*;
+
+@Configuration
+public class AppConf2 {
+    @Autowired
+    private MemberDao memberDao;
+    @Autowired
+    private MemberPrinter memberPrinter;
+
+    @Bean
+    public MemberRegisterService memberRegisterService() {
+        return new MemberRegisterService(memberDao);
+    }
+
+    @Bean
+    public ChangePasswordService changePasswordService() {
+        ChangePasswordService passwordService =
+            new ChangePasswordService();
+        passwordService.setMemberDao(memberDao);
+
+        return passwordService;
+    }
+
+    @Bean
+    public MemberListPrinter listPrinter() {
+        return new MemberListPrinter(memberDao, memberPrinter);
+    }
+
+    @Bean
+    public MemberInfoPrinter infoPrinter() {
+        MemberInfoPrinter infoPrinter =
+            new MemberInfoPrinter();
+        infoPrinter.setMemberDao(memberDao);
+        infoPrinter.setPrinter(memberPrinter);
+
+        return infoPrinter;
+    }
+
+    @Bean
+    public VersionPrinter versionPrinter() {
+        VersionPrinter versionPrinter =
+            new VersionPrinter();
+        versionPrinter.setMajorVersion(5);
+        versionPrinter.setMinorVersion(0);
+
+        return versionPrinter;
+    }
+
+}
