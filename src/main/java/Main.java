6cf8d5d95e2922dc79f8ffdf379a1a1dfb2e056d
Jaeryong Im <hackerrs.im@gmail.com>
2019-04-19 16:26:13 +0900

    Chapter06 빈 라이프사이클과 범위

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,133 +1,22 @@
-import assembler.Assembler;
 import config.AppCtx;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import spring.*;
+import org.springframework.context.support.AbstractApplicationContext;
+import spring.Client;
+import spring.Client2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
 
-    private static ApplicationContext ctx = null;
+    public static void main(String[] args) {
+        AbstractApplicationContext ctx =
+            new AnnotationConfigApplicationContext(AppCtx.class);
 
-    public static void main(String[] args) throws IOException {
-        ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+        Client client = ctx.getBean(Client.class);
+        Client2 client2 = ctx.getBean(Client2.class);
 
-        BufferedReader reader =
-            new BufferedReader(new InputStreamReader(System.in));
+        client.send();
+        client2.send();
 
-        while (true) {
-            System.out.println("명령어를 입력하세요: ");
-            String command = reader.readLine();
-
-            if (command.equalsIgnoreCase("exit")) {
-                System.out.println("종료합니다.");
-                break;
-            }
-
-            if (command.startsWith("new ")) {
-                processNewCommand(command.split(" "));
-                continue;
-            } else if (command.startsWith("change ")) {
-                processChangeCommand(command.split(" "));
-                continue;
-            } else if (command.equals("list")) {
-                processListCommand();
-                continue;
-            } else if (command.startsWith("info ")) {
-                processInfoCommand(command.split(" "));
-                continue;
-            } else if (command.equals("version")) {
-                processVersionCommand();
-                continue;
-            }
-
-            printHelp();
-        }
-    }
-
-    private static void processVersionCommand() {
-        VersionPrinter versionPrinter =
-            ctx.getBean("versionPrinter", VersionPrinter.class);
-        versionPrinter.print();
-    }
-
-    private static void processInfoCommand(String[] args) {
-        if (args.length != 2) {
-            printHelp();
-            return;
-        }
-        MemberInfoPrinter infoPrinter =
-            ctx.getBean("infoPrinter", MemberInfoPrinter.class);
-        infoPrinter.printMemberInfo(args[1]);
-    }
-
-    private static Assembler assembler = new Assembler();
-
-    private static void processNewCommand(String[] args) {
-        System.out.println((args.length));
-        if (args.length != 5) {
-            printHelp();
-            return;
-        }
-
-        MemberRegisterService registerService =
-            ctx.getBean(MemberRegisterService.class);
-
-        RegisterRequest req = new RegisterRequest();
-        req.setEmail(args[1]);
-        req.setName(args[2]);
-        req.setPassword(args[3]);
-        req.setConfirmPassword(args[4]);
-
-        if (!req.isPasswordEqualToConfirmPassword()) {
-            System.out.println("암호와 확인이 일치하지 않습니다.");
-            return;
-        }
-
-        try {
-            registerService.regist(req);
-            System.out.println("등록했습니다.\n");
-        } catch (DuplicateMemberException e) {
-            System.out.println("이미 존재하는 이메일입니다. \n");
-        }
-
-    }
-
-    private static void processChangeCommand(String[] args) {
-        System.out.println((args.length));
-        if (args.length != 4) {
-            printHelp();
-            return;
-        }
-
-        ChangePasswordService changePasswordService =
-            ctx.getBean(ChangePasswordService.class);
-
-        try {
-            changePasswordService.changePassword(args[1], args[2], args[3]);
-            System.out.println("암호를 변경했습니다. \n");
-        } catch (MemberNotFoundException e) {
-            System.out.println("존재하지 않는 이메일입니다. \n");
-        } catch (WrongIdPasswordException e) {
-            System.out.println("이메일과 암호가 일치하지 않습니다. \n");
-        }
-    }
-
-    private static void processListCommand() {
-        MemberListPrinter printer =
-            ctx.getBean("listPrinter", MemberListPrinter.class);
-        printer.printAll();
-    }
-
-    private static void printHelp() {
-        System.out.println();
-        System.out.println("잘못된 명령입니다.아래 명령어 사용법을 확인하세요.");
-        System.out.println("명령어 사용법");
-        System.out.println("new 이메일 이름 암호 암호확인");
-        System.out.println("change 이메일 현재비번 변경비번");
-        System.out.println();
+        ctx.close();
     }
 }
--- src/main/java/config/AppCtx.java
+++ src/main/java/config/AppCtx.java
@@ -1,33 +1,25 @@
 package config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import spring.*;
+import spring.Client;
+import spring.Client2;
 
 @Configuration
-@ComponentScan(basePackages = { "spring" })
 public class AppCtx {
     @Bean
-    @Qualifier("printer")
-    public MemberPrinter memberPrinter1() {
-        return new MemberPrinter();
+    public Client client() {
+        Client client = new Client();
+        client.setHost("host");
+
+        return client;
     }
 
-    @Bean
-    @Qualifier("summaryPrinter")
-    public MemberSummaryPrinter memberPrinter2() {
-        return new MemberSummaryPrinter();
-    }
+    @Bean(initMethod = "connect", destroyMethod = "close")
+    public Client2 client2() {
+        Client2 client2 = new Client2();
+        client2.setHost("host");
 
-    @Bean
-    public VersionPrinter versionPrinter() {
-        VersionPrinter versionPrinter =
-            new VersionPrinter();
-        versionPrinter.setMajorVersion(5);
-        versionPrinter.setMinorVersion(0);
-
-        return versionPrinter;
+        return client2;
     }
 }
